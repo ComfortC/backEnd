d1ae3ea2bb12f7529e2e1d76aef9ababef6ef819
ComfortC <comfortchino@gmail.com>
2016-08-24 18:26:40 +0200

    Tests

---
+++ app/src/test/java/com/example/khumalo/backend/UtilsTest.java
@@ -0,0 +1,27 @@
+package com.example.khumalo.backend;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by KHUMALO on 8/23/2016.
+ */
+public class UtilsTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+
+    }
+
+    public void testIsFoundAlongTheJourney() throws Exception {
+
+    }
+
+    public void testIsInThePath() throws Exception {
+
+    }
+}
